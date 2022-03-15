105410a469acd538c8a54470fe2a61ff499bbe2e
Sena Nur Yeşilyurt <61209847+senanuryesilyurt@users.noreply.github.com>
2022-03-15 11:58:21 +0300

    Create Delete.sql

---
+++ Delete.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee 
+WHERE id=1
+
+DELETE FROM employee
+WHERE name='Esin Yurt'
+
+DELETE FROM employee
+WHERE email='gcolrein1d@goo.ne.jp'
+
+DELETE FROM employee
+WHERE birthday='2005-12-18'
+
+DELETE FROM employee
+WHERE id=20
